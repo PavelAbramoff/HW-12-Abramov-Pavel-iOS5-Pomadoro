d3f062192592ac1f9c989d0d05b239cc1805a72c
PavelAbramoff <96954222+PavelAbramoff@users.noreply.github.com>
2022-05-04 23:12:50 +0400

    Timer-Pomodoro #2

--- HW-12-Abramov-Pavel-iOS5-Pomadoro/ViewController.swift
+++ HW-12-Abramov-Pavel-iOS5-Pomadoro/ViewController.swift
@@ -8,145 +8,170 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    let leassonLable: UILabel = {
-        let label = UILabel()
-        label.text = "My Timer"
-        label.font = UIFont.boldSystemFont(ofSize: 24)
-        label.textColor = .systemRed
-        label.numberOfLines = 0
-        label.textAlignment = .center
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
     
-    let shapeView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(named: "elips")
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        return imageView
-    }()
-    
-    let timerLable: UILabel = {
-        let label = UILabel()
-        label.text = "1"
-        label.font = UIFont.boldSystemFont(ofSize: 50)
-        label.textColor = .systemRed
-        label.numberOfLines = 1
-        label.textAlignment = .center
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    let startButton: UIButton = {
-        let button = UIButton()
-        button.layer.cornerRadius = 35
-        button.setTitle("START", for: .normal)
-        button.backgroundColor = .green
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-    
-    var timer = Timer()
-    let shapeLayer = CAShapeLayer()
-    var durationTimer = 20
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        self.animationCircular()
+    enum IntervalType {
+      case Pomodoro
+      case RestBreak
     }
+    
+    var intervals: [IntervalType] = [.Pomodoro,
+                                     .RestBreak,
+                                     .Pomodoro,
+                                     .RestBreak,
+                                     .Pomodoro,
+                                     .RestBreak,
+                                     .Pomodoro]
+    
+    var currentInterval = 0
+    let pomodoroIntervalTime = 10
+    let restBreakIntervalTime = 5
+    var timeRemaining = 0
+    var timer = Timer()
+    
+    @IBOutlet weak var minutesLabel: UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
+    @IBOutlet weak var intervalLabel: UILabel!
+    @IBOutlet weak var startPauseButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var shapeViev: UIImageView!
+    
+    @IBAction func startPauseButton(_ sender: Any) {
+        basivAnimation()
+        if timer.isValid {
+            
+            startPauseButton.setTitle("Resume", for: .normal)
+          resetButton.isEnabled = true
+          pauseTimer()
+        } else {
+            
+            startPauseButton.setTitle("Pause", for: .normal)
+          resetButton.isEnabled = false
+          if currentInterval == 0 && timeRemaining == pomodoroIntervalTime {
+             startNextInterval()
+          } else {
+                startTimer()
+          }
+        }
+      }
+    
+     @IBAction func resetButton(_ sender: Any) {
+        if timer.isValid {
+           timer.invalidate()
+        }
+        resetToBegining()
+      }
+
+   let shapeLayer = CAShapeLayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+         animationCircular()
+         resetToBegining()
+    }
         
-        timerLable.text = "\(durationTimer)"
-        view.backgroundColor = .blue
-        setConstraints()
-        startButton.addTarget(self, action: #selector(startButtonTapped), for: .touchUpInside)
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
     
-    @objc func startButtonTapped() {
-        
-        basivAnimation()
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
+    func resetToBegining() {
+        currentInterval = 0
+        intervalLabel.text = "Готов"
+        startPauseButton.setTitle("Start", for: .normal)
+        resetButton.isEnabled = false
+        timeRemaining = pomodoroIntervalTime
+        updateDisplay()
     }
     
-    @objc func timerAction(){
-        
-        durationTimer -= 1
-        timerLable.text = "\(durationTimer)"
-        print(durationTimer)
-        
-        if durationTimer == 0 {
-            timer.invalidate()
-            durationTimer = 20
-            timer.invalidate()
-            
+    func startNextInterval() {
+      if currentInterval < intervals.count {
+        if intervals[currentInterval] == .Pomodoro {
+          timeRemaining = pomodoroIntervalTime
+          intervalLabel.text = "За работу !"
+        } else {
+           timeRemaining = restBreakIntervalTime
+           intervalLabel.text = "Отличная работа! Отдыхай"
         }
+        updateDisplay()
+        startTimer()
+        currentInterval += 1
+      } else {
+        resetToBegining()
     }
+  }
+    
+  func updateDisplay() {
+    let (minutes, seconds) = minutesAndSeconds(from: timeRemaining)
+    minutesLabel.text = formatMinuteOrSecond(minutes)
+    secondLabel.text = formatMinuteOrSecond(seconds)
+  }
+    
+  func startTimer() {
+      basivAnimation()
+        timer = Timer.scheduledTimer(timeInterval: 2,
+                                     target: self,
+                                     selector: #selector(timerTick),
+                                     userInfo: nil,
+                                     repeats: true)
+  }
+    
+  @objc func timerTick() {
+  if timeRemaining > 0 {
+    timeRemaining -= 1
+    print("time: \(timeRemaining)")
+    updateDisplay()
+   } else {
+    timer.invalidate()
+    startNextInterval()
+   }
+ }
+    
+  func pauseTimer() {
+    timer.invalidate()
+    intervalLabel.text = "Paused."
+  }
+
+  func minutesAndSeconds(from seconds: Int) -> (Int, Int) {
+    return (seconds / 60, seconds % 60)
+  }
+      
+  func formatMinuteOrSecond(_ number: Int) -> String {
+    return String(format: "%02d", number)
+  }
     
     // MARK: Animation
     
-    func animationCircular(){
-        
-        let center = CGPoint(x: shapeView.frame.width / 2, y: shapeView.frame.height / 2)
-        let endAngle = (-CGFloat.pi / 2)
-        let startAngle = 2 * CGFloat.pi + endAngle
-        let circularPath = UIBezierPath(arcCenter: center, radius: 120, startAngle: startAngle, endAngle: endAngle, clockwise: false)
-        
-        shapeLayer.path = circularPath.cgPath
-        shapeLayer.lineWidth = 80
-        shapeLayer.fillColor = UIColor.clear.cgColor
-        shapeLayer.strokeEnd = 1
-        shapeLayer.lineCap = CAShapeLayerLineCap.round
-        shapeLayer.strokeColor = UIColor.gray.cgColor
-        shapeView.layer.addSublayer(shapeLayer)
-    }
-    
-    func basivAnimation() {
-        
-        let basivAnimation = CABasicAnimation(keyPath: "strokeEnd")
-        
-        basivAnimation.toValue = 0
-        basivAnimation.duration = CFTimeInterval(durationTimer)
-        basivAnimation.fillMode = CAMediaTimingFillMode.forwards
-        basivAnimation.isRemovedOnCompletion = true
-        shapeLayer.add(basivAnimation, forKey: "basivAnimation")
+  func animationCircular() {
+
+    let center = CGPoint(x: shapeViev.frame.width / 2,
+                             y: shapeViev.frame.height / 2)
+
+    let endAngle = (-CGFloat.pi / 2)
+    let startAngle = 2 * CGFloat.pi + endAngle
+    let circularPath = UIBezierPath(arcCenter: center,
+                                        radius: 112,
+                                        startAngle: startAngle,
+                                        endAngle: endAngle,
+                                        clockwise: false)
+
+    shapeLayer.path = circularPath.cgPath
+    shapeLayer.lineWidth = 62
+    shapeLayer.fillColor = UIColor.clear.cgColor
+    shapeLayer.strokeEnd = 1
+    shapeLayer.lineCap = CAShapeLayerLineCap.round
+    shapeLayer.strokeColor = UIColor.link.cgColor
+    shapeViev.layer.addSublayer(shapeLayer)
+  }
+
+  func basivAnimation() {
+
+    let basivAnimation = CABasicAnimation(keyPath: "strokeEnd")
+
+    basivAnimation.toValue = 0
+    basivAnimation.duration = CFTimeInterval(pomodoroIntervalTime)
+    basivAnimation.fillMode = CAMediaTimingFillMode.forwards
+    basivAnimation.isRemovedOnCompletion = true
+    shapeLayer.add(basivAnimation, forKey: "basivAnimation")
     }
 }
 
-extension ViewController {
-    
-    func setConstraints() {
-    
-    view.addSubview(leassonLable)
-    NSLayoutConstraint.activate([
-        leassonLable.topAnchor.constraint(equalTo: view.topAnchor,constant: 150),
-        leassonLable.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 20),
-        leassonLable.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant:  -20),
-    ])
-         
-        view.addSubview(shapeView)
-        NSLayoutConstraint.activate([
-            shapeView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            shapeView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            shapeView.heightAnchor.constraint(equalToConstant: 300),
-            shapeView.widthAnchor.constraint(equalToConstant: 300)
-        ])
-        
-        shapeView.addSubview(timerLable)
-        NSLayoutConstraint.activate([
-            timerLable.centerXAnchor.constraint(equalTo: shapeView.centerXAnchor),
-            timerLable.centerYAnchor.constraint(equalTo: shapeView.centerYAnchor)
-            
-        ])
-        
-    view.addSubview(startButton)
-    NSLayoutConstraint.activate([
-        startButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100),
-        startButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-        startButton.heightAnchor.constraint(equalToConstant: 70),
-        startButton.widthAnchor.constraint(equalToConstant: 300)
-    ])
-                                              
-    }
-}
